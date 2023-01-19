4cf7a66e7a78b151daeb6cec9be2a167ecdf24bb
n-cekic <ncekic13@gmail.com>
2023-01-19 22:25:54 +0100

    refactor

--- helloWorld.py
+++ helloWorld.py
@@ -27,9 +27,9 @@ print(82 * '_')
 print('init\t\ttime\tinertia\thomo\tcompl\tv-meas\tARI\tAMI\tsilhouette')
 
 
-def bench_k_means(estimator, name, data):
+def bench_k_means(estimator, name, benchmark_data):
     t0 = time()
-    estimator.fit(data)
+    estimator.fit(benchmark_data)
     print('%-9s\t%.2fs\t%i\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f'
           % (name, (time() - t0), estimator.inertia_,
              metrics.homogeneity_score(labels, estimator.labels_),
@@ -37,15 +37,16 @@ def bench_k_means(estimator, name, data):
              metrics.v_measure_score(labels, estimator.labels_),
              metrics.adjusted_rand_score(labels, estimator.labels_),
              metrics.adjusted_mutual_info_score(labels,  estimator.labels_),
-             metrics.silhouette_score(data, estimator.labels_,
+             metrics.silhouette_score(benchmark_data, estimator.labels_,
                                       metric='euclidean',
                                       sample_size=sample_size)))
 
+
 bench_k_means(KMeans(init='k-means++', n_clusters=n_digits, n_init=10),
-              name="k-means++", data=data)
+              name="k-means++", benchmark_data=data)
 
 bench_k_means(KMeans(init='random', n_clusters=n_digits, n_init=10),
-              name="random", data=data)
+              name="random", benchmark_data=data)
 
 
 print(82 * '_')
