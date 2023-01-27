59b5fd50d216505ee0db4142a0bd0873ce812662
n-cekic <ncekic13@gmail.com>
2023-01-27 21:43:40 +0100

    remove exes code

--- gui.py
+++ gui.py
@@ -39,23 +39,7 @@ class GUI:
 
         self.window = QWidget(flags=Qt.WindowFlags())
         self.window.setWindowTitle("Number Recognition")
-
         self.window.setGeometry(100, 100, 400, 330)
-
-        self.hw_label = QLabel("<h1>Hello, World!</h1>")
-        self.hw_label.setParent(self.window)
-        self.hw_label.move(15, 60)
-        self.hw_label.setFixedSize(200, 200)
-        self.hw_label.setAlignment(Qt.AlignCenter)
-        self.hw_label.setScaledContents(True)
-
-        self.image_label = QLabel()
-
-        self.display_image_btn = QPushButton("Display Message")
-        self.display_image_btn.setParent(self.window)
-        self.display_image_btn.setGeometry(220, 10, 90, 25)
-        self.display_image_btn.setStyleSheet("text_align: center")
-        self.display_image_btn.clicked.connect(self.set_message)
 
         self.text_field = QLineEdit()
         self.text_field.setGeometry(10, 10, 380, 25)
